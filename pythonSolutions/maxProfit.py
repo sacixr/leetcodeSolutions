015b84a072bdfde54db5717bd039b190f7881411
Wik <zkac176@live.rhul.ac.uk>
2023-11-16 12:46:00 +0000

    Add example for maxProfit solution

--- pythonSolutions/maxProfit.py
+++ pythonSolutions/maxProfit.py
@@ -3,7 +3,7 @@ You are given an array prices where prices[i] is the price of a given stock on t
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.'''
 
-def maxProfit(self, prices) -> int:
+def maxProfit(prices) -> int:
     # set minimum price to be the first price
     min_price = prices[0]
     max_profit = 0
@@ -19,3 +19,12 @@ def maxProfit(self, prices) -> int:
 
     # return max profit
     return max_profit
+
+# If you would like to see this code working, please run the file and make sure you have python installed.
+prices = [7,1,5,3,6,4]
+print("If prices = ", prices)
+print("Maximum Profit is:", maxProfit(prices))
+
+prices = [7,6,4,3,1]
+print("\nIf prices = ", prices)
+print("Maximum Profit is:", maxProfit(prices))
